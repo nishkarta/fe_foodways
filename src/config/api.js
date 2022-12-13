a661f1bde5cc74698e1760f9b0d1112fae9290a6
Riri Sukmaresa <77942358+nishkarta@users.noreply.github.com>
2022-12-13 10:32:19 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const API = axios.create({
-    baseURL: 'https://foodways.herokuapp.com/api/v1',
+    baseURL: 'https://nishwaysfood.up.railway.app/api/v1',
 
 })
 
